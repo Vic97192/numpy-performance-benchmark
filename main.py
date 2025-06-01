a7ce48506d7d5d25a62ab163e307243ecfadf1d0
李宣毅 Lee Hsuan-Yi <vic97192@gmail.com>
2025-06-01 20:17:16 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
 # h = x * y
-
+# h有很多筆。1 2 3 4 5 6 7 8 代表 1 2 3 4 是x的，5 6 7 8是y的
 import numpy as np
 import time
 
 f = open("array_data.txt",'r',encoding="utf-8")
-# TODO: 資料以八筆為單位。1 2 3 4 5 6 7 8 代表 1 2 3 4 是x的，5 6 7 8是y的. 但前提是要先讀出來到flist
-flist = 0 # 這是讀出來的資料。要修改並刪減
+flist = f.readlines()
+flist = flist[0].split(" ")
+flist = list(map(float,flist))
+
 x,y = [],[]
 
-for i in range(0,0,0): # TODO: 修改range()
-    for j in range(0): # TODO: 修改range()
-    # TODO: 把屬於x和屬於y的分別加進x和y
-        pass #寫完把這行刪掉
+for i in range(0,len(flist)-4,8):
+    for j in range(4):
+        x.append(flist[i + j])
+        y.append(flist[i +(j+4)])
 
+'/Users/viclee/Downloads/CompOrg_5_Large and Fast-Exploiting Memory Hierarchy.pdf'
 h = []
-
-before = 0 # TODO: 用time模組取得“精確時間”
+j = 0
+before = time.time()
 for j in range(len(x)):
     h.append(x[j]*y[j])
-after = 0 # TODO: 用time模組取得“精確時間”
+#print(h)
+after = time.time()
 
-print(f"Execution time:{after - before}")
+print(f"Execution time:{after-before}")
 
-#變數重置
+
 i = 0
 h = []
 
-# TODO: 把 h = x * y 用numpy重做一遍。只有相乘的部分要計時。Hint: numpy可以直接用矩陣“元素相乘”
+x = np.array(x)
+y = np.array(y)
+before = time.time()
+h.append(x*y)
+after = time.time()
+print(after-before)
